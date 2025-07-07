6f20e8b1a95f2cc060d17d0df49196282861704e
JinMori07 <griffith7207@gmail.com>
2025-07-07 14:19:57 +0200

    early version

---
+++ JinsCrosshair/CrosshairWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JinsCrosshair
+{
+    /// <summary>
+    /// Interaction logic for CrosshairWindow.xaml
+    /// </summary>
+    public partial class CrosshairWindow : Window
+    {
+        public CrosshairWindow(BitmapImage image, double width, double height, double opacity)
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+
+            this.Width = width;
+            this.Height = height;
+
+            receivedImage.Source = image;
+            receivedImage.Width = width;
+            receivedImage.Height = height;
+            receivedImage.Opacity = opacity;
+
+
+            SystemEvents.DisplaySettingsChanged += SystemEvents_DisplaySettingsChanged;
+
+            this.Closed += (s, e) => SystemEvents.DisplaySettingsChanged -= SystemEvents_DisplaySettingsChanged;
+        }
+
+        private void SystemEvents_DisplaySettingsChanged(object sender, EventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                CenterWindowOnScreen();
+            });
+        }
+
+        private void CenterWindowOnScreen()
+        {
+            var screenWidth = SystemParameters.PrimaryScreenWidth;
+            var screenHeight = SystemParameters.PrimaryScreenHeight;
+
+            this.Left = (screenWidth - this.ActualWidth) / 2;
+            this.Top = (screenHeight - this.ActualHeight) / 2;
+        }
+
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            int exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, exStyle | WS_EX_TRANSPARENT | WS_EX_LAYERED);
+        }
+
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_EX_TRANSPARENT = 0x00000020;
+        private const int WS_EX_LAYERED = 0x00080000;
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hwnd, int index, int value);
+    }
+}
---
+++ JinsCrosshair/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using Microsoft.Win32;
+using WpfAnimatedGif;
+
+namespace JinsCrosshair
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var KanekiBannerGif = new BitmapImage(new Uri("pack://application:,,,/Images/kaneki.gif"));
+            ImageBehavior.SetAnimatedSource(KanekiBanner, KanekiBannerGif);
+        }
+
+        private void WindowTop_MouseButton(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void SelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(selectedFilePath);
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+
+                crosshair_image.Source = bitmap;
+            }
+        }
+
+        private void ConfirmImage_Click(object senderm, RoutedEventArgs e)
+        {
+            if (crosshair_image.Source is BitmapImage selectedBitmap)
+            {
+                double height = crosshair_image.Height;
+                double width = crosshair_image.Width;
+                double opacity = crosshair_image.Opacity;
+
+                CrosshairWindow crosshairWindow = new CrosshairWindow(selectedBitmap, width, height, opacity);
+                crosshairWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please load a image first");
+            }
+        }
+
+        private void QuitApp_click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimizeApp_click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void SliderOpacity_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (crosshair_image==null)
+            {
+                return;
+            }
+
+            double value = e.NewValue;
+
+            crosshair_image.Opacity = value / 100;
+        }
+
+        private void SliderSize_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (crosshair_image == null)
+            {
+                return;
+            }
+
+            double scalefactor = 20;
+            double value = e.NewValue * scalefactor;
+
+            crosshair_image.Width = value;
+            crosshair_image.Height = value;
+        }
+    }
+}
